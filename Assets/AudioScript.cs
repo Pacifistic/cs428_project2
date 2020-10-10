4106422d2e38ab552f4f53e88df2a0df6ffeea1e
Dominykas Sipelis <dsipel2@uic.edu>
2020-10-10 12:33:16 -0500

    alpha completed

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    private AudioSource line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<AudioSource>();
+    }
+
+    void sayLine() {
+        line.PlayOneShot(line.clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ChangeAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ChangeAnim : MonoBehaviour
+{
+
+    public GameObject button;
+    public Animator dominyk;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    void OnButtonPressed(VirtualButtonBehaviour vb) {
+        Debug.Log("Button Pressed");
+        dominyk.SetBool("Button", true);
+    }
+
+    void OnButtonReleased(VirtualButtonBehaviour vb) {
+        Debug.Log("Button Released");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
