0a98852e0a60bbb37f64dcb9e89f979313ffd76e
Dominykas Sipelis <dsipel2@uic.edu>
2020-10-14 17:04:07 -0500

    added colliders and spawning balls on click

---
+++ Assets/ballSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballSpawn : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 randomPosDelta;
+
+        if (Input.GetMouseButton(0) || Input.touchCount > 0) {
+            randomPosDelta = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+
+            Instantiate(ballPrefab, startPos.position + randomPosDelta, startPos.rotation);
+        }
+    }
+}
---
+++ Assets/destroyBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyBall : MonoBehaviour
+{
+    public float lifetime = 5.0f; // default lifetime of 5 seconds
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (lifetime > 0) {
+            lifetime -= Time.deltaTime;
+            if (lifetime <= 0) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
