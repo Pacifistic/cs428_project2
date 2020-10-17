54ad7cbe5f6a14ec2f941f69ae65459998d762c7
Dominykas Sipelis <dsipel2@uic.edu>
2020-10-17 12:15:18 -0500

    finished life-size version

--- Assets/ballSpawn.cs
+++ Assets/ballSpawn.cs
@@ -6,6 +6,7 @@ public class ballSpawn : MonoBehaviour
 {
     public GameObject ballPrefab;
     public Transform startPos;
+    public float scale = .5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,10 +19,10 @@ public class ballSpawn : MonoBehaviour
     {
         Vector3 randomPosDelta;
 
-        //if (Input.GetMouseButton(0) || Input.touchCount > 0) {
-            randomPosDelta = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(-0.5f, 0.5f));
+        if (Input.GetMouseButton(0) || Input.touchCount > 0) {
+            randomPosDelta = new Vector3(Random.Range(-scale, scale), 0, Random.Range(-scale, scale));
 
             Instantiate(ballPrefab, startPos.position + randomPosDelta, startPos.rotation);
-        //}
+        }
     }
 }
